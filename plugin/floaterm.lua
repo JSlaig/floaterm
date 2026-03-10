309a14f2fff5df16d0719f42da9fc6100a919f7f
JSlaig <jsaligz@gmail.com>
2026-03-10 17:32:18 +0100

    test opennew term command

--- plugin/floaterm.lua
+++ plugin/floaterm.lua
@@ -1,3 +1,10 @@
 vim.api.nvim_create_user_command("FloatermToggle", function()
   require("floaterm").toggle()
 end, {})
+
+vim.api.nvim_create_user_command("FloatermOpenInNewTerm", function()
+  require("floaterm").open()
+  require("floaterm.api").new_term()
+end, {})
+
+
